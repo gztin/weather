a8bbedbc01cabf8ac732b846bbfcacf90929ffb0
GGT <atharsfake@gmail.com>
2020-09-28 18:30:25 +0800

    update
    更新

--- mary/play.js
+++ mary/play.js
@@ -24,21 +24,25 @@ $('.putMoney').click(function(){
 
 // 下注
 $('.bet-option').click(function(){
-	if(parseInt($('.inf-credit').text()) == 0){
-		// 偵測餘額是否足夠下注
-		alert("餘額不足，請先投幣");
+	if(ending==true){
+		console.log("目前無法下注");
 	}else{
-		betCredit = betCredit - 1;
-		betTotal = betTotal + 1;
-		$('.inf-credit').text(betCredit);
-		// 取得目前選取的位置
-		var x =$(this).index();
-		// 讀取目前已經下注的金額，並將值回傳至陣列中
-		betCount = bet[x];
-		betCount = betCount + 1;
-		bet[x] = betCount;
-		$(".sub-inf").eq(x).find("span.betInf").text(betCount);
-		console.log("bet陣列:"+bet);
+		if(parseInt($('.inf-credit').text()) == 0){
+			// 偵測餘額是否足夠下注
+			alert("餘額不足，請先投幣");
+		}else{
+			betCredit = betCredit - 1;
+			betTotal = betTotal + 1;
+			$('.inf-credit').text(betCredit);
+			// 取得目前選取的位置
+			var x =$(this).index();
+			// 讀取目前已經下注的金額，並將值回傳至陣列中
+			betCount = bet[x];
+			betCount = betCount + 1;
+			bet[x] = betCount;
+			$(".sub-inf").eq(x).find("span.betInf").text(betCount);
+			// console.log("bet陣列:"+bet);
+		}
 	}
 });
 
@@ -80,30 +84,39 @@ $('.cash').click(function(){
 
 
 $('.win').click(function(){
-	if(ending == true){
+	if((betCredit==0)&&(betMoney==0)){
+		console.log("目前無任何餘額");
+	}else{
+		// 進入結束模式
+		ending = true;
+		
 		// 將本金轉移到押注的金額
-		if(betCredit >= 0){
+		if(betCredit > 0){
 			betMoney = betMoney + 1;
 			betCredit = betCredit - 1;
 			$('.inf-bonus').text(betMoney);
 			$('.inf-credit').text(betCredit);
 			
-			console.log("credit的值是:"+betCredit);
+			console.log("betCredit的值是:"+betCredit);
 			console.log("betMoney的值是:"+betMoney);
 		}
 		else{
-			credit = 0;
+			betCredit = 0;
 			console.log("betCredit的值是已經是:"+betCredit+"，無法繼續");
 		}
-	}else{
-		console.log("遊戲尚未開始");
 	}
+	
 	
 });
 $('.credit').click(function(){
-	if( ending == true){
+	if((betCredit==0)&&(betMoney==0)){
+		console.log("目前無任何餘額");
+	}else{
+		// 進入結束模式
+		ending = true;
+
 		// 將押注的金額轉移到本金
-		if(betMoney >= 0){
+		if(betMoney > 0){
 			betMoney = betMoney -1;
 			betCredit = betCredit + 1;
 			$('.inf-bonus').text(betMoney);
@@ -113,9 +126,8 @@ $('.credit').click(function(){
 		}
 		else{
 			betMoney = 0;
+			console.log("betMoney的值是已經是:"+betMoney+"，無法繼續");
 		}
-	}else{
-		console.log("遊戲尚未開始");
 	}
 });
 
@@ -131,63 +143,77 @@ function playGame(){
 		move = move - 1;
 		highLight = highLight - 1;
 		// 跑分數判定
-		if((highLight==1)||(highLight==10)||(highLight==22)){
+		if((highLight===1)||(highLight===10)||(highLight===22)){
 			// 跑到橘子，x1
 			console.log("小瑪莉結果是橘子!獎金1倍");
 			betMoney = bet[6];
 			betMoney = Math.floor(betMoney*1);
 			$('.inf-bonus').text(betMoney);
 			$('.sub-inf').eq(6).css({'animation-iteration-count':'infinite'});
+			ending = true;
+			console.log("遊戲結束!");
 		}
-		else if((highLight==2)||(highLight==13)||(highLight==21)){
+		else if((highLight===2)||(highLight===13)||(highLight===21)){
 			// 跑到鈴鐺，x2
 			console.log("小瑪莉結果是鈴鐺!獎金2倍");
 			betMoney = bet[4];
 			betMoney = Math.floor(betMoney*2);
 			$('.inf-bonus').text(betMoney);
 			$('.sub-inf').eq(4).css({'animation-iteration-count':'infinite'});
+			ending = true;
+			console.log("遊戲結束!");
 		}
-		else if((highLight==3)||(highLight==4)||(highLight==5)){
+		else if((highLight===3)||(highLight===4)||(highLight===5)){
 			// 跑到BAR，x3
 			console.log("小瑪莉結果是Bar!獎金1倍");
 			betMoney = bet[0];
 			betMoney = Math.floor(betMoney*1);
 			$('.inf-bonus').text(betMoney);
 			$('.sub-inf').eq(0).css({'animation-iteration-count':'infinite'});
+			ending = true;
+			console.log("遊戲結束!");
 		}
-		else if((highLight==6)||(highLight==9)||(highLight==12)||(highLight==14)){
+		else if((highLight===6)||(highLight===9)||(highLight===12)||(highLight===14)){
 			// 跑到蘋果，x5
 			console.log("小瑪莉結果是蘋果!獎金5倍");
 			betMoney = bet[7];
 			betMoney = Math.floor(betMoney*5);
 			$('.inf-bonus').text(betMoney);
 			$('.sub-inf').eq(7).css({'animation-iteration-count':'infinite'});
+			ending = true;
+			console.log("遊戲結束!");
 		}
-		else if((highLight==7)||(highLight==18)||(highLight==19)){
+		else if((highLight===7)||(highLight===18)||(highLight===19)){
 			// 跑到檸檬，x1
 			console.log("小瑪莉結果是檸檬!獎金1倍");
 			betMoney = bet[5];
 			betMoney = Math.floor(betMoney*1);
 			$('.inf-bonus').text(betMoney);
 			$('.sub-inf').eq(5).css({'animation-iteration-count':'infinite'});
+			ending = true;
+			console.log("遊戲結束!");
 		}
-		else if((highLight==8)||(highLight==24)){
+		else if((highLight===0)||(highLight===8)||(highLight===24)){
 			// 跑到西瓜，x1
 			console.log("小瑪莉結果是西瓜!獎金1倍");
 			betMoney = bet[3];
 			betMoney = Math.floor(betMoney*1);
 			$('.inf-bonus').text(betMoney);
 			$('.sub-inf').eq(3).css({'animation-iteration-count':'infinite'});
+			ending = true;
+			console.log("遊戲結束!");
 		}
-		else if((highLight==11)||(highLight==20)){
+		else if((highLight===11)||(highLight===20)){
 			// 跑到777，x7
 			console.log("小瑪莉結果是777!獎金7倍");
 			betMoney = bet[1];
 			betMoney = Math.floor(betMoney*7);
 			$('.inf-bonus').text(betMoney);
 			$('.sub-inf').eq(1).css({'animation-iteration-count':'infinite'});
+			ending = true;
+			console.log("遊戲結束!");
 		}
-		else if((highLight==15)||(highLight=23)){
+		else if((highLight===15)||(highLight===23)){
 			// 跑到Lucky，再玩一次，功能開發中
 			// move = 0;
 			// sec = 50;
@@ -196,21 +222,24 @@ function playGame(){
 			// betMoney = bet[3];
 			// betMoney = Math.floor(betMoney*1);
 		}
-		else if((highLight==16)||(highLight==17)){
+		else if((highLight===16)||(highLight===17)){
 			// 跑到星星，x10
 			console.log("小瑪莉結果是星星!獎金10倍");
 			betMoney = bet[2];
 			betMoney = Math.floor(betMoney*10);
 			$('.inf-bonus').text(betMoney);
 			$('.sub-inf').eq(2).css({'animation-iteration-count':'infinite'});
-		}else{
+			ending = true;
+			console.log("遊戲結束!");
+		}
+		else{
 			// 如果都不是，就是沒中獎
 			$('.inf-bonus').text(betMoney);
 			console.log("沒中獎");
+			ending = true;
+			console.log("遊戲結束!");
 		}
-
-		ending = true;
-		console.log("遊戲結束!");
+		
 	}
 	else{
 		if( move+12 > gameResult){
@@ -252,10 +281,23 @@ function initial(){
 	$('.inf-credit').text(0);
 	$('.play:input').attr('disable',true);
 	$('.play:input').css({'cursor':'not-allowed'});
+
+	// 下注區禁止操作
+	$('.win:input').attr('disable',true);
+	$('.win:input').css({'cursor':'not-allowed'});
+	$('.credit:input').attr('disable',true);
+	$('.credit:input').css({'cursor':'not-allowed'});
 }
 function startGame(){
 	$('.play:input').removeAttr('disable');
 	$('.play:input').css({'cursor':'pointer'});
+
+	$('.win:input').removeAttr('disable');
+	$('.win:input').css({'cursor':'pointer'});
+
+	$('.credit:input').removeAttr('disable');
+	$('.credit:input').css({'cursor':'pointer'});
+
 	$('.light').css({'animation-iteration-count':'1'});
 	$('.sub-inf').css({'animation-iteration-count':'1'});
 }
